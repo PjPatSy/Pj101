6f92f71fd74907e9f0cb719d2b3a76db1edcdce0
pat42410 <pat42410@hotmail.fr>
2014-12-22 19:05:45 +0100

    optimisations

--- src/sat.cpp
+++ src/sat.cpp
@@ -67,7 +67,35 @@ bool cherche(vector<val_t> & valeurs, var_t suiv, const cnf_t & cnf) {
 			return false;
 		}
 	}
-} 
+}
+
+//~ bool cherche(vector<val_t> & valeurs, var_t suiv, const cnf_t & cnf, const vector<vector<cls_t> > & index) {
+	//~ while(valeurs[suiv] != INDETERMINEE){
+		//~ suiv++;
+	//~ }
+	//~ if(suiv >= valeurs.size()){
+		//~ return valeur_cnf(valeurs, cnf) == VRAI;
+	//~ }else{
+		//~ vector<var_t> p1 = propage(var2lit(suiv, true), valeurs, index);
+		//~ if(p1.size() > 0){
+			//~ if(cherche(valeurs, suiv, cnf, index)){
+				//~ return true;
+			//~ }
+		//~ }
+		//~ for(size_t i = 0; i < p1.size(); i++){
+			//~ valeurs[p1[i]] = INDETERMINEE;
+		//~ }
+		//~ vector<var_t> p2 = propage(var2lit(suiv, false), valeurs, index);	
+		//~ if(p2.size() > 0){
+			//~ if(cherche(valeurs, suiv, cnf, index)){
+				//~ return true;
+			//~ }
+		//~ }
+		//~ for(size_t i = 0; i < p2.size(); i++){
+			//~ valeurs[p2[i]] = INDETERMINEE;
+		//~ }
+	//~ }	
+//~ }
 
 vector<vector<cls_t>> indexe_clauses(const cnf_t& cnf) {
 	vector<vector<cls_t> > v;
@@ -107,44 +135,50 @@ bool contient_insatisfaite(var_t variable, const vector<val_t>& valeurs, const v
 	return false;
 }
 
-//~ vector<var_t> propage(lit_t lit, vector<val_t> & valeurs, cnf_t & cnf, vector<vector<cls_t> > & index) {
-	//~ // A FAIRE
-	//~ vector<var_t> v;
-	//~ return v;
-//~ }
+vector<var_t> propage(lit_t lit, vector<val_t> & valeurs, const vector<vector<cls_t> > & index) {
+	vector<var_t> v_var;
+	vector<lit_t> v_lit(1,lit);
+	var_t v;
+	lit_t l;
+	
+	// Temps qu'il reste des littéraux à traiter
+	while(!v_lit.empty()){
+		// On retire un littérale
+		l = v_lit.back();
+		v_lit.pop_back();
+		
+		v = lit2var(l);
+		// Si le littérale n'a pas de valeur
+		if(valeurs[v] == INDETERMINEE){
+			// On ajoute la variable
+			v_var.push_back(v);
+			// On affecte la bonne valeur à la variable
+			if(positif(l)){
+				valeurs[v] = FAUX;
+			}else{
+				valeurs[v] = VRAI;
+			}
+			
+			// On utilise l'index pour accéder aux clauses à vérifier
+			for(size_t i = 0; i < index[l].size(); i++){
+				// Pour chaque clause unitaire trouvée,
+				// on ajoute le littéral qui n'a pas de valeur dans les littéraux à traiter
+				//~ rien bité je regarderai plus tard
+				//~ v_lit.push_back(le litteral);
 
-// Fonction propage d'un autre gars
-//~ vector<var_t> propage(lit_t lit, vector<val_t> & valeurs, const vector<vector<cls_t> > & index) {
-	//~ vector<var_t> resultat;
-	//~ vector<lit_t> liste (1,lit);      // --> Cette structure contiendras la liste courante                                                  
-	//~ //     des littéraux que l'on veut affecter à vrai                                                    
-	//~ lit_t lit_temp;                   // --> Littéral temporaire de travail                                                                 
-	//~ var_t var_temp;                   // --> Variable associé au littéral temp                                                              
-	//~ while(!liste.empty()) {           // --> Tant qu'il reste des littéraux à traiter                                                       
-		//~ lit_temp = liste.back();
-		//~ liste.pop_back();            // --> On retire un littéral des littéraux à traiter                                                  
-		//~ var_temp = lit2var(lit_temp);
-		//~ if(valeurs[var_temp] == INDETERMINEE) {    // --> Si aucune valeur ne lui ait affecté                                              
-			//~ resultat.push_back(var_temp);         // --> On ajoute la variable correspondant à temp dans le vector résultat               
-			//~ positif(lit_temp) ? valeurs[var_temp] = FAUX : valeurs[var_temp] = VRAI;
-			//~ // --> Pour rendre unitaire une clause, il faut que l'un de ses littéraux soit affecté à FAUX                                 
-			//~ // --> Pour chaque clause                                                                                                     
-			//~ for(size_t i=0; i<index[lit_temp].size(); i++) {
-				//~ // --> Si une clause s'évalue à FAUX alors la CNF aussi                                                                  
-				//~ if(valeur_clause(valeurs, index[lit_temp][i]) == FAUX) {
-					//~ for(size_t j=0; j<resultat.size(); j++)
-						//~ valeurs[resultat[j]] = INDETERMINEE;
-					//~ resultat.clear();
-					//~ return resultat;
-				//~ }
-//~ 
-				//~ lit_t t = estUnitaire(index[lit_temp][i],valeurs);
-				//~ // --> Pour chaque clause unitaire trouvée                                                                               
-				//~ if(t != -1) {
-					//~ liste.push_back(t);
-				//~ }
-			//~ }
-		//~ }
-	//~ }
-	//~ return resultat;
-//~ }
+				
+				// Si la clause s'évalue à FAUX
+				if(valeur_clause(valeurs, index[l][i]) == FAUX){
+					// On remet à INDETERMINEE les valeurs des variables
+					for(size_t j = 0; j < v_var.size(); j++){
+						valeurs[v_var[j]] = INDETERMINEE;
+					}
+					// On renvoie le vecteur vide
+					v_var.clear();
+					return v_var;
+				}
+			}
+		}
+	}
+	return v_var;
+}
